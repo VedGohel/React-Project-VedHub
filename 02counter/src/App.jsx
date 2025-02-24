15df323f104c0499645eec806bcf8f2e6f78f5b2
VedGohel <vedgohel85@gmail.com>
2025-02-25 01:02:20 +0530

    React-Projects-VedHub

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  //useState Hooks Using Counter Update
+  //let counter = 0
+  let[counter , setCounter] = useState(0)
+
+ 
+ //addValue functions Woking if Statement 
+ // Maximum range is 20 times.
+  const addValue = () => {
+   if(counter<20){
+
+    //counter = counter + 1 
+    setCounter(counter + 1)
+    console.log("clicked", counter); 
+   }
+  }
+
+  //removeValue functions working but prevent 
+  // a counter like negative value.
+  const removeValue = () => {
+    if(counter>0){
+    setCounter(counter - 1)
+    console.log("clicked", counter);  
+    }
+  }
+   
+  return (
+    <>
+      <h1>Chai Aur React | Ved Gohel</h1>
+      <h2>Count Value : {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add Value</button>
+      <br/>
+      <br/>
+      <button
+      onClick={removeValue}
+      >Remove Value</button> 
+    </>
+  )
+}
+
+export default App
